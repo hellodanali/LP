6ab5e72311ef713a2168232988d556e86cc2da01
Dana <danagotmail@gmail.com>
2018-03-12 19:20:14 -0700

    make navbar stick to top

--- src/view/LandingPage/components/Navbar.js
+++ src/view/LandingPage/components/Navbar.js
@@ -6,22 +6,24 @@ import RequestDemoButton from '../common/RequestDemoButton';
 class Navbar extends Component {
 	render() {
 		return (
-			<nav className="navbar-container">
-				<img className="bc-logo" src={logo} />
+			<nav className="navbar">
+				<div className="navbar-container">
+					<img className="bc-logo" src={logo} />
 
-				<ul className="nav-menu">
-					<li className="nav-item">For GCs</li>
-					<li className="nav-item">For Subs</li>
-					<li className="nav-item">For Owners</li>
-					<li className="nav-item">About</li>
-					<li className="nav-item login">Login</li>
-					<li className="nav-item">
-						<RequestDemoButton className={'request-demo-nav'}>
-							Request Demo
-						</RequestDemoButton>
-					</li>
-					<li className="icon">&#9776;</li>
-				</ul>
+					<ul className="nav-menu">
+						<li className="nav-item">For GCs</li>
+						<li className="nav-item">For Subs</li>
+						<li className="nav-item">For Owners</li>
+						<li className="nav-item">About</li>
+						<li className="nav-item login">Login</li>
+						<li className="nav-item">
+							<RequestDemoButton className={'request-demo-nav'}>
+								Request Demo
+							</RequestDemoButton>
+						</li>
+						<li className="icon">&#9776;</li>
+					</ul>
+				</div>
 			</nav>
 		);
 	}
